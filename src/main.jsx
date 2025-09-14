179eeeddbd47ddf7340c1d40f321db9019f14577
S.Rahul <s.rahul5116@outlook.com>
2025-09-14 14:27:22 +0530

    Routing Added 📌

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,32 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+import SignIn from './auth/SignIn.jsx'
+import { Home } from './Pages/Home.jsx'
+import Dashboard from './Pages/Dashboard.jsx'
 
+const router = createBrowserRouter([
+  {
+    element: <App />,
+    children: [
+      {
+        path :'/',
+        element : <Home />
+      },
+      {
+        path :'/dashboard',
+        element : <Dashboard />
+      }
+    ]
+  },
+  {
+    path:'/auth/sign-in',
+    element:<SignIn />
+  }
+])
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider  router={router} />
   </StrictMode>,
 )
